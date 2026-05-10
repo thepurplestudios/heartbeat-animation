99d38312ac33ac588497bb2627a4cd3b454bb6cd
Dia Soni <purplestudios7@gmail.com>
2026-05-10 15:48:20 +0530

    Create project shell and layout

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-white">Heartbeat Animation</h1>
+    </main>
+  );
+}
+
+export default App;
